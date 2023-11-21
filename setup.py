f834a14b87b22be20593cca6d6c57936de1496ae
stepan.nadym <stepan.nadym@gmail.com>
2023-11-21 01:06:32 +0100

    fix

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     author='Stepan Orlov',
+    description='A library for converting naming styles in Python code.',
+    long_description='''The writing_style_converter library provides convenient tools for converting between various naming styles (e.g., snake_case, kebab_case, camel_case, pascal_case) in Python code. Streamline the process of handling different naming conventions in models, APIs, and more. Check out the [GitHub repository](https://github.com/OrlovSA/writing_style_converter/blob/main/README.md) for usage examples and detailed documentation. Telegram @Orlov_SA''',
+    long_description_content_type='text/markdown',
     author_email='stepan.nadym@gmail.com',
-    description='Telegram @Orlov_SA',
     license="MIT License"
 )
